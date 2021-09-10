7035378d99f88d8f485bd0f1202472138321884f
anandsamy81 <anand.samy@gmail.com>
2021-09-10 16:25:53 +0800

    initial Commit

---
+++ BookingApi/Data/ApiDBContext.cs
@@ -0,0 +1,19 @@
+﻿using BookingApi.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApi.Data
+{
+    public class ApiDBContext:DbContext
+    {
+        public ApiDBContext(DbContextOptions<ApiDBContext> options)
+        : base(options)
+        {
+        }
+
+        public DbSet<Booking> Booking { get; set; }
+    }
+}
---
+++ BookingApi/Models/Booking.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookingApi.Models
+{
+    public class Booking
+    {
+        [Key]
+        public int BookingId { get; set; }
+
+        public DateTime BookingDate { get; set; }
+
+        public DateTime BookingTIme { get; set; }
+        public string venue { get; set; }
+
+        public string Screen { get; set; }
+        public string MovieName { get; set; }
+
+        public int NoofSeats { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
